5b5b59e19a7b23c73fb0e6dd7d12ffa7a0a14b31
istimaldar <istimaldar@gmail.com>
2019-02-06 16:44:36 +0300

    Third version. Broken

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const os = require('os');
 const express = require('express');
 const app = express();
 const applicationName = process.env.DUMMY_NAME || "Some random app";
-const version = "2";
+const version = "3";
+let requestsCounter = 0;
 
 app.get('/', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send({hostname: os.hostname(), application: applicationName, version: version});
+    if (requestsCounter >= 7) {
+        res.status(500).send("I'm broken. Sorry.")
+    } else {
+        res.set('Content-Type', 'application/json');
+        res.send({hostname: os.hostname(), application: applicationName, version: version});
+    }
+    requestsCounter++;
 });
 
 app.listen(8080, function () {
