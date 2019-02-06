d7633479706b3c4e330126283669b844de04fa3d
istimaldar <istimaldar@gmail.com>
2019-02-06 17:00:58 +0300

    Fourth version. Fixed

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const process = require('process');
 const os = require('os');
 const express = require('express');
-const app = express();
+
 const applicationName = process.env.DUMMY_NAME || "Some random app";
-const version = "3";
-let requestsCounter = 0;
+const version = "4";
+
+const app = express();
 
 app.get('/', function (req, res) {
-    if (requestsCounter >= 7) {
-        res.status(500).send("I'm broken. Sorry.")
-    } else {
-        res.set('Content-Type', 'application/json');
-        res.send({hostname: os.hostname(), application: applicationName, version: version});
-    }
-    requestsCounter++;
+    res.set('Content-Type', 'application/json');
+    res.send({hostname: os.hostname(), application: applicationName, version: version});
 });
 
 app.listen(8080, function () {
