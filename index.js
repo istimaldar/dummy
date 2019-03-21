efa231560a46cb825ec06284580079cd14c35d40
istimaldar <istimaldar@gmail.com>
2019-03-21 11:15:39 +0300

    Fifth version. Completely broken.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const process = require('process');
-const os = require('os');
 const express = require('express');
 
 const applicationName = process.env.DUMMY_NAME || "Some random app";
-const version = "4";
 
 const app = express();
 
 app.get('/', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send({hostname: os.hostname(), application: applicationName, version: version});
+    res.status(500).send("I'm broken. Sorry.")
 });
 
 app.listen(8080, function () {
